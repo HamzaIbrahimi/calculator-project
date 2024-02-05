921833312ca208c913840f92af9002412220cc13
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-02-05 08:50:18 +0100

    calculator now works with basic math

--- script.js
+++ script.js
@@ -1,50 +1,105 @@
-const buttons = document.querySelectorAll('button')
-const screen_area = document.querySelector('.screen_area')
-const clearButton = document.querySelector('#clear')
-const nine = document.querySelector('#nine');
-const add = document.querySelector('#add');
+const numberButtons = document.querySelectorAll('.number')
+const allButtons = document.querySelectorAll('button');
+const screen_area = document.querySelector('.screen_area');
+const clearButton = document.querySelector('#clear');
+const grey_buttons = document.querySelector('.grey_numbers')
+const equal = document.querySelector('#equal');
+const operatorButtons = document.querySelector('.orange_operators');
+let num1;
+let num2;
+let operator = '';
+let stringNumber = '';
+let result;
 
 
-function displayNumbersOnScreen () {//Function to display numbers in text area
-    buttons.forEach((button) => {
-        button.addEventListener('click', (e) => {
-          return  screen_area.textContent += e.currentTarget.textContent;
-          
-        })
-    }) 
+function displayNumber() { //Function that displays number and saves it variable one
+  grey_buttons.addEventListener("click", (e) => {
+    if (e.target && e.target.className == "number") {
+      stringNumber += e.target.textContent;
+      screen_area.textContent = stringNumber;
+      console.log(stringNumber);
+    }
+  });
 }
 
-function addition (e) {
-    let number1 = parseInt(e.currentTarget.textContent);
-    screen_area.textContent = number1;
 
+function saveOperator() {
+  operatorButtons.addEventListener("click", (e) => {
+    if (e.target && e.target.className == "operator") {
+        if(stringNumber !== '') {
+          num1 = parseInt(stringNumber);
+          stringNumber = '';
+        }
+        operator = e.target.textContent;
+    }
+    if (screen_area.textContent == result) {
+      num1 = parseInt(result);
+    }
+  });
 }
 
-add.addEventListener('click',addition )
 
+//Mathematical Formulas for the Calculator
+const add = (a,b) => a+b;
+const subtract = (a,b) => a-b;
+const multiply = (a,b) => a*b;
+const divide = (a,b) => a/b;
 
+//Write a function that takes 2 numbers and operator and 
+//spits a solution based on operator 
 
-function fullCalculator () {
-    displayNumbersOnScreen();
-    
-
+function useMath (num1 , operator, num2) {
+    if (operator == '+') return add(num1,num2);
+    else if (operator == '-') return subtract(num1,num2); 
+    else if (operator == 'x') return multiply(num1,num2);
+    else if(operator == '÷') return divide(num1,num2);
 }
-fullCalculator();
+
+
+function calculate () {
+  num2 = parseInt(stringNumber);
+  result = useMath(num1, operator, num2);
+  screen_area.textContent = result;
+ 
+}
 
 
 
+displayNumber();
+saveOperator();
+equal.addEventListener('click', calculate);
 
 
 
 //function to clear screen
 function clearScreen () {
     screen_area.textContent = '';
+    stringNumber = '';
+
 }
+
 clearButton.addEventListener('click', clearScreen);
 
 
 
 
+//add grey background on hover remove on mouse out
+allButtons.forEach((button) => {
+    button.addEventListener('mouseover', () => {
+      button.style.background = '#D3D3D3';
+    })
+    button.addEventListener('mouseout', () => {
+        button.style.background = '';
+      })
+    
+}) 
 
+
+// Reduce screen text on more than 10 clicks
+// counter++; overflow reduction
+//  if (counter > 10) {
+//       let newSize = 50 - ((counter-10));
+//      screen_area.style.fontSize = `${newSize}px`
+//  }
 
 
